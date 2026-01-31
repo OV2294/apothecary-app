72c0925e109d8b9164cddb67f1d6731f021ea9a4
Omkar Vaidya <vaidyaomkar2209@gmail.com>
2026-01-31 08:55:10 +0530

    updated password field

--- auth.js
+++ auth.js
@@ -187,3 +187,20 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+const toggles = document.querySelectorAll('.toggle-pass');
+
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            const input = toggle.previousElementSibling;
+
+            if (input.type === 'password') {
+                input.type = 'text';
+                toggle.classList.remove('fa-eye');
+                toggle.classList.add('fa-eye-slash');
+            } else {
+                input.type = 'password';
+                toggle.classList.remove('fa-eye-slash');
+                toggle.classList.add('fa-eye');
+            }
+        });
+    });
